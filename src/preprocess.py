68b69dcca695128ac505ba392be3a241784613e8
Daniel <daniwero2003@gmail.com>
2025-02-20 14:56:45 +0100

    Implemented stopword removal (broken), lemmatisation and stemming. NO TESTING DONE

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,13 +1,20 @@
 import nltk
 import spacy
 import re
+import pandas as pd
 
 from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from nltk.stem.porter import PorterStemmer
 
 
 
 #TODO: Experiment with different tokenizers
-    
+
+nltk.download("stopwords")
+nltk.download("wordnet")
+
 
 def remove_urls(text):
     # Matches HTTP(S) and WWW URLs
@@ -23,41 +30,72 @@ def sentence_splitting(text):
     sentences = sent_tokenize(text)  # Uses Punkt by default
     return sentences
 
-def remove_stopwords():
+def remove_stopwords(tokens, language):
+    stop_words = set(stopwords.words(language))
+    return [token for token in tokens if token not in stop_words]
+
+def perform_lemmatization(tokens):
+    lemmatizer = WordNetLemmatizer()
+    return [lemmatizer.lemmatize(token) for token in tokens]
+
+def perform_stemming(tokens):
+    stemmer = PorterStemmer()
+    return [stemmer.stem(token) for token in tokens]
     pass
 
-# Rule based suffix removal
-def perform_lemmatisation():  # british spelling
-    pass
-
-# Dictionary linguistical database 
-def perform_stemming():
-    pass
-
-def preprocess(sentence, labels):
-    '''
+def preprocess(sentence, labels, **kwargs):
+    """
     Task: Given a sentence apply all the required preprocessing steps
     to compute train our classifier, such as sentence splitting, 
     tokenization or sentence splitting.
 
     Input: Sentence in string format
     Output: Preprocessed sentence either as a list or a string
-    '''
+    """
+    do_stopwords = kwargs.get('remove_stopwords', False)
+    do_lemmatize = kwargs.get('do_lemmatize', False)
+    do_stemming = kwargs.get('do_stemmin', False)
+    stopword_lang = kwargs.get('stopword_lang', "english")
+
     # Step 1: Perform URL Regex removal
-    # Not needed probably --  Dani: I think we should remove URLs
     _sentence = sentence.apply(remove_urls)
-    
-    # Step 2: Perform tokenization
+
+    # Step 2: Perform sentence splitting (punkt)
+    _sentence = _sentence.apply(sentence_splitting)
+
+    new_sentences = []
+    new_labels = []
+
+    for sent, label in zip(_sentence, labels):
+        if isinstance(sent, list):
+            new_sentences.extend(sent)
+            new_labels.extend([label] * len(sent))
+        else:  # There was no splitting
+            new_sentences.append(sent)
+            new_labels.append(label)
+
+    # Convert from list back to pd Series
+    _sentence = pd.Series(new_sentences)
+    _labels = new_labels
+
+    # Step 3: Perform tokenization (word)
     _sentence = _sentence.apply(word_tokenize)
 
-    # Step 3: Remove capitalization
+    # Step 4: Remove capitalization
     _sentence = _sentence.str.lower()
 
+    # Step 5: Lematisation (optional)
+    # if do_stopwords:
+    #     _sentence = _sentence.apply(remove_stopwords)
+    # TODO BROKEN: somehow needs to parse language into stopword function
+    # Still tho, I don't really see a need to remove stopwords for langdetect?
 
-    # Place your code here
-    # Keep in mind that sentence splitting affectes the number of sentences
-    # and therefore, you should replicate labels to match.
-    return _sentence,labels
+    # Step 6: Lematisation (optional)
+    if do_lemmatize:
+        _sentence = _sentence.apply(perform_lemmatization)
 
+    # Step 7: Stemming (optional)
+    if do_stemming:
+        _sentence = _sentence.apply(perform_stemming)
 
-
+    return _sentence, _labels
