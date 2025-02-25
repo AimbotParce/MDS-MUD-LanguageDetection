22007050c605383ee8296bec305ddac6d5f0f553
Daniel <daniwero2003@gmail.com>
2025-02-25 16:26:22 +0100

    The best plotting parameters ever. Use PDF

---
+++ src/plotter.py
@@ -0,0 +1,26 @@
+import matplotlib as mpl
+
+
+mpl.rcParams['font.family'] = 'serif'
+mpl.rcParams['font.sans-serif'] = 'Computer Modern Sans Serif'
+mpl.rcParams['text.usetex'] = True
+mpl.rcParams['text.latex.preamble'] = r'\usepackage{amsmath}'
+# mpl.rcParams['text.latex.preamble'] = r'\usepackage{cm-super}'
+mpl.rcParams['font.weight'] = 1000
+mpl.rcParams['mathtext.fontset'] = 'cm'
+mpl.rcParams['font.size'] = 16
+mpl.rcParams['legend.fontsize'] = 'small'
+mpl.rcParams['axes.titlesize'] = 'x-large'
+mpl.rcParams['axes.labelsize'] = 'large'
+mpl.rcParams['xtick.labelsize'] = 'small'
+mpl.rcParams['ytick.labelsize'] = 'small'
+mpl.rcParams['axes.titlepad'] = 10
+mpl.rcParams['axes.grid'] = True
+mpl.rcParams['axes.grid.which'] = 'both'
+mpl.rcParams['grid.alpha'] = 0.5
+mpl.rcParams['grid.color'] = 'k'
+mpl.rcParams['grid.linestyle'] = ':'
+mpl.rcParams['grid.linewidth'] = 0.5
+mpl.rcParams['savefig.format'] = 'pdf'
+mpl.rcParams['axes.labelpad'] = 4
+
