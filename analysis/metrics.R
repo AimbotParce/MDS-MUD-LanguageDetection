466261be6e0a74cb7ec0e740cd1515f96da89c2a
eZWALT <waltertheshadow333@gmail.com>
2025-03-02 19:26:12 +0100

    some plots R

--- analysis/metrics.R
+++ analysis/metrics.R
@@ -27,8 +27,44 @@ df <- raw[, -which(names(raw) %in% c("dataset", "vectorizer", "lemmatize", "stem
 summary(df)
 condes(df, num.var = which(names(df) == "f1_weighted"))
 
+# Coverage as a function of tokenization
 plot(df$test_coverage ~ df$tokenizer)
+
+
+#F1 score as a function of test / tokenization / classifier 
 plot(df$f1_weighted ~ df$test_coverage)
 plot(df$f1_weighted ~ df$tokenizer)
 plot(df$f1_weighted ~ df$classifier)
-boxplot(df$f1_weighted ~ df$tokenizer + df$classifier, col = df$classifier)
+
+
+# combined factor levels (tokenizer + classifier)
+df <- df %>%
+  mutate(group = interaction(tokenizer, classifier))
+df <- df %>%
+  mutate(group = reorder(group, as.numeric(tokenizer)))
+
+
+# Plot F1 as a function of tokenizer + classifier
+boxplot(f1_weighted ~ group, 
+        data = df, 
+        las = 2,                         # Rotate x-axis labels
+        cex.axis = 0.7,
+        main = "F1 Weighted by Tokenizer and Classifier",
+        xlab = "Tokenizer + Classifier", 
+        ylab = "F1 Weighted",
+        col = rainbow(length(unique(df$group)))
+ )
+
+
+# Group by tokenization method
+tokenizer_colors <- rainbow(length(unique(df$tokenizer)))
+df$group_color <- tokenizer_colors[as.factor(df$tokenizer)]
+boxplot(f1_weighted ~ group, 
+        data = df, 
+        las = 2,                         # Rotate x-axis labels
+        cex.axis = 0.7,
+        main = "F1 Weighted by Tokenizer and Classifier",
+        xlab = "Tokenizer + Classifier", 
+        ylab = "F1 Weighted",
+        col = df$group_color  # Color based on tokenizer
+)
